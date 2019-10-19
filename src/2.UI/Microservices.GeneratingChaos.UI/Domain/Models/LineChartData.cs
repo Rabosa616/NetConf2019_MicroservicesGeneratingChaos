5dd1db8f4cca09f29059a5437b9f0d09790faefe
miguel molina <miguel.molina@erni-espana.es>
2019-10-19 08:54:41 +0200

    Add UI Domain Models Add UI Extensions Delete Old Models

---
+++ src/2.UI/Microservices.GeneratingChaos.UI/Domain/Models/LineChartData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microservices.GeneratingChaos.UI.Domain.Models
+{
+    public class LineChartData
+    {
+        public DateTime xValue { get; set; }
+        public double temperatureValue { get; set; }
+        public double humidityValue { get; set; }
+        public double pressureValue { get; set; }
+    }
+}
--- src/2.UI/Microservices.GeneratingChaos.UI/Startup.cs
+++ src/2.UI/Microservices.GeneratingChaos.UI/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microservices.GeneratingChaos.UI.Data;
+using Microservices.GeneratingChaos.UI.Extensions;
 
 namespace Microservices.GeneratingChaos.UI
 {
@@ -27,7 +27,8 @@ namespace Microservices.GeneratingChaos.UI
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+
+            services.AddHttp(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
